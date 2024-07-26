62ada5615cbc2096ed4ccc3e403dfa6a725e717e
Mohammed Sadique <m.sadique04@icloud.com>
2024-07-26 08:09:01 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState } from "react";
 import React, { Component } from "react";
 import Plx from "react-plx";
-import NavBar from './Components/NavBar';
-import HeroSection from './Components/HeroSection';
+import NavBar from "./Components/NavBar";
+import HeroSection from "./Components/HeroSection";
+import FooterSection from "./Components/Footer/FooterSection";
 function App() {
   const parallaxData = [
     {
@@ -20,18 +21,21 @@ function App() {
 
   return (
     <>
-      <NavBar/>
-      
-      <div className='plx-effect'>
+      <NavBar />
+      <div className="plx-effect">
         <Plx className="MyAwesomeParallax" parallaxData={parallaxData}>
-          <div className='ellipse1'></div>
-          <div className='ellipse2'></div>
-          <HeroSection/>
+          <div className="ellipse1"></div>
+          <div className="ellipse2"></div>
+          <HeroSection />
         </Plx>
       </div>
-      <div className='dummy'></div>
+      <div className="plx-effect">
+        <div className="ellipse1"></div>
+        <div className="ellipse2"></div>
+        <FooterSection />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
