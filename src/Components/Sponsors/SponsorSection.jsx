85960f23306b15d9b6182bebe20ef5cdef165eb9
Mohammed Sadique <m.sadique04@icloud.com>
2024-08-01 12:28:44 +0530

    sasa

---
+++ src/Components/Sponsors/SponsorSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './SponsorSection.css'
+const SponsorSection = () => {
+  return (
+    <div className='hero-section'>
+      <div className='hero-content'>
+        <h4 className='society-name'>GFG ADGIPS Chapter</h4>
+        <p className='d-para'>PRESENTS</p>
+        <h1 className='eventName'><span>Live</span><br/> The Code</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, expedita!</p>
+        <button className='register-btn'>Register</button>
+      </div>
+      <div className='hero-banner'>
+      </div>
+    </div>
+  )
+}
+
+export default SponsorSection
